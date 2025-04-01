e95789c67ccc55987f3af2fa8047509da8384bc3
Clemens Kohl <clemens.kohl@heimspiel.de>
2025-04-01 23:56:29 +0200

    feat(lsp): add lazydev.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,15 @@
 return {
+	{ -- lazydev.nvim is a plugin that properly configures LuaLS for editing your Neovim config by lazily updating your workspace libraries.
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 	{
 		-- Main LSP Configuration
 		"neovim/nvim-lspconfig",
