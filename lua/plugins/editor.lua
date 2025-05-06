6d5e3bf2e88fd3e6ffb717ad297114b2599994cd
Clemens Kohl <clemens.kohl@heimspiel.de>
2025-05-06 17:16:58 +0200

    feat(editor): add diffview and flash.nvim + update plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -172,4 +172,19 @@ return {
 			})
 		end,
 	},
+	{
+		"sindrets/diffview.nvim",
+		keys = {
+			{
+				"<leader>do",
+				vim.cmd.DiffviewOpen,
+				desc = "[d]iffview [o]pen",
+			},
+			{
+				"<leader>dc",
+				vim.cmd.DiffviewClose,
+				desc = "[d]iffview [c]lose",
+			},
+		},
+	},
 }
