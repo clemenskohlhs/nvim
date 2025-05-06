f0f5f8162e4a59d8a9dadede0c8071dd251bcaa9
Clemens Kohl <clemens.kohl@heimspiel.de>
2025-05-06 17:15:02 +0200

    feat(lsp): add lemminx xml lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -218,6 +218,7 @@ return {
 					filetypes = { "json", "jsonc", "cnf" },
 				}, --json
 				pyright = {}, --python
+				lemminx = {}, --xml
 				-- intelephense = {}, --php
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
