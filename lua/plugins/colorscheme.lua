87aa360fc6f2134dce907fe11076536532d76300
Clemens Kohl <clemens.kohl@heimspiel.de>
2025-07-04 23:17:06 +0200

    feat(colorscheme): add more colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	-- 	priority = 1000, -- make sure to load this before all the other start plugins
-	-- 	config = function()
-	-- 		-- load the colorscheme here
-	-- 		vim.cmd([[colorscheme catppuccin-mocha]])
-	-- 	end,
-	-- },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- config = function()
+		-- 	-- load the colorscheme here
+		-- 	vim.cmd([[colorscheme catppuccin-mocha]])
+		-- end,
+	},
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
@@ -17,5 +17,41 @@ return {
 		config = function()
 			vim.cmd([[colorscheme tokyonight-storm]])
 		end,
+	},
+	{
+		"marko-cerovac/material.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		-- config = function()
+		-- 	vim.cmd([[colorscheme material-palenight]])
+		-- end,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		-- config = function()
+		-- 	vim.cmd([[colorscheme onedark]])
+		-- end,
+	},
+	{
+		"nickkadutskyi/jb.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		-- config = function()
+		-- 	vim.cmd([[colorscheme jb]])
+		-- end,
+	},
+	{
+		"ribru17/bamboo.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		-- config = function()
+		-- 	vim.cmd([[colorscheme bamboo]])
+		-- end,
 	},
 }
