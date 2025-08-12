f467d06172d3f137e9a56de02f65127cd1fa6964
Clemens Kohl <clemens.kohl@heimspiel.de>
2025-08-12 11:36:21 +0200

    feat(lsp): add ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -219,6 +219,7 @@ return {
 				}, --json
 				pyright = {}, --python
 				lemminx = {}, --xml
+				ts_ls = {}, --typescript
 				-- intelephense = {}, --php
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
